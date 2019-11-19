26f416c86481cad432ab69b0fafd6b57014c0cce
maninikki <manikantaallanki500000@gmail.com>
2019-11-19 15:51:35 +0530

    done at last

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import multiplication
 import div
 import module
 print("<-- Addition subtraction Program -->\n1.Addition\n2.Subtraction\n3.multiplication\n4.division\n5.modulus")
-inp = int(input("Enter your choice-->"))
+inp = input("Enter your choice-->")
 
 if(inp==1):
 	print(add.add())
