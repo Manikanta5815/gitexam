1a80ed806e088d074fb83c778a12946be9b13ed8
maninikki <manikantaallanki500000@gmail.com>
2019-11-19 15:34:45 +0530

    finnaly done

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 #This is main file to run PYthon file.
 import add
 import subtraction
-
-print("<-- Addition subtraction Program -->\n1.Addition\n2.Subtraction")
+import multiplication
+import div
+print("<-- Addition subtraction Program -->\n1.Addition\n2.Subtraction\n3.multiplication\n4.division")
 inp = int(input("Enter your choice-->"))
 
 if(inp==1):
 	print(add.add())
 elif(inp==2):
-	print(subtraction.subt())
+	print(subtraction.sub())
+elif(inp==3):
+	print(multiplication.multiply())
+elif(inp==4):
+	print(div.div())
 else:
 	print("Wrong choice")
