4ca55e562c0a632fd2afac83a790aa07b73b62cd
maninikki <manikantaallanki500000@gmail.com>
2019-11-19 15:45:10 +0530

    finally done

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import add
 import subtraction
 import multiplication
 import div
-print("<-- Addition subtraction Program -->\n1.Addition\n2.Subtraction\n3.multiplication\n4.division")
+import module
+print("<-- Addition subtraction Program -->\n1.Addition\n2.Subtraction\n3.multiplication\n4.division\n5.modulus")
 inp = int(input("Enter your choice-->"))
 
 if(inp==1):
@@ -14,5 +15,7 @@ elif(inp==3):
 	print(multiplication.multiply())
 elif(inp==4):
 	print(div.div())
+elif(inp==5):
+	print(module.modulo())
 else:
 	print("Wrong choice")
